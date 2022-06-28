13e27e69b370861b16fa8774feeca1519e38c97e
Anthony Whitfield <aww91@yahoo.com>
2022-06-28 17:23:25 -0400

    Initial code

--- BirdCall/ViewController.swift
+++ BirdCall/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
@@ -13,7 +15,71 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    var audioPlayer: AVAudioPlayer!
+    
+    
+    @IBAction func buttonpressed(_ sender: UIButton) {
+    
+    //Play Sounds when button pressed
+        // Cardinal Bird
+        if sender.tag == 0 {
+        if let sound = NSDataAsset(name: "sound0"){
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch{
+                print("ERROR: \(error.localizedDescription) Could not initialize AVAudioPlayer object for Cardinal Bird.")
+            }
+           
+        }else{
+            print("ERROR: Could not read data from file sound0")
+        }}
+        // Mallard Duck Call
+        if sender.tag == 1 {
+        if let sound = NSDataAsset(name: "sound1"){
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch{
+                print("ERROR: \(error.localizedDescription) Could not initialize AVAudioPlayer object for Mallard Duck Call.")
+            }
+           
+        }else{
+            print("ERROR: Could not read data from file sound0")
+        }}
+        // Dove
+        if sender.tag == 2 {
+        if let sound = NSDataAsset(name: "sound2"){
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch{
+                print("ERROR: \(error.localizedDescription) Could not initialize AVAudioPlayer object Dove.")
+            }
+           
+        }else{
+            print("ERROR: Could not read data from file sound0")
+        }}
+        // Western SandPiper
+        if sender.tag == 3 {
+        if let sound = NSDataAsset(name: "sound3"){
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch{
+                print("ERROR: \(error.localizedDescription) Could not initialize AVAudioPlayer object Western SandPiper.")
+            }
+           
+        }else{
+            print("ERROR: Could not read data from file sound0")
+        }}
+   
+    
+    
+    
+    
+    
+    
+    }
 }
 
